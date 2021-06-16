81e710649313e89c29b95547e414cd150fad9c1d
tengda <303571072@qq.com>
2021-06-16 22:46:59 +0200

    2-8 add custom config and get value

--- src/main/java/com/tengda/wiki/controller/TestController.java
+++ src/main/java/com/tengda/wiki/controller/TestController.java
@@ -1,15 +1,19 @@
 package com.tengda.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+//  read from config file, if no value, take TEST
+  @Value("${name: TEST}")
+  private String testName;
 
   @GetMapping ("/hello")
   public String helloWorld() {
-    return "Hello world !";
+    return "Hello world !" + this.testName;
   }
 
   @PostMapping("/hello/post")
