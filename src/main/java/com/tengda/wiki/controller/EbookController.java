f677464a2299b16adcaee477101080712c36626b
tengda <303571072@qq.com>
2021-06-24 09:00:57 +0200

    3-6 add CommonResp class & refactor ebook list api

--- src/main/java/com/tengda/wiki/controller/EbookController.java
+++ src/main/java/com/tengda/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.tengda.wiki.controller;
 
 import com.tengda.wiki.domain.Ebook;
+import com.tengda.wiki.resp.CommonResp;
 import com.tengda.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,17 @@ public class EbookController {
   @Resource
   private EbookService ebookService;
 
+//  @GetMapping ("/list")
+//  public List<Ebook> list() {
+//    return ebookService.list();
+//  }
   @GetMapping ("/list")
-  public List<Ebook> list() {
-    return ebookService.list();
+  public CommonResp list() {
+    CommonResp<List<Ebook>>  resp = new CommonResp<>();
+    List<Ebook> list = ebookService.list();
+    resp.setContent(list);
+    return resp;
   }
+
 
 }
