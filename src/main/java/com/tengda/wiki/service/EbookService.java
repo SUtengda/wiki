47e949d7661b9270055f61878fc882598ceef15e
tengda <303571072@qq.com>
2021-06-25 23:08:26 +0200

    3-8 add copyUtil

--- src/main/java/com/tengda/wiki/service/EbookService.java
+++ src/main/java/com/tengda/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.tengda.wiki.domain.EbookExample;
 import com.tengda.wiki.mapper.EbookMapper;
 import com.tengda.wiki.req.EbookReq;
 import com.tengda.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.tengda.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,12 +21,13 @@ public class EbookService {
     EbookExample.Criteria criteria = ebookExample.createCriteria();
     criteria.andNameLike("%" + req.getName() + "%");
     List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-    List<EbookResp> resqList = new ArrayList<>();
-    for (Ebook ebook: ebookList) {
-      EbookResp ebookResq = new EbookResp();
-      BeanUtils.copyProperties(ebook,ebookResq);
-      resqList.add(ebookResq);
-    }
+//    List<EbookResp> resqList = new ArrayList<>();
+//    for (Ebook ebook: ebookList) {
+//      EbookResp ebookResq = new EbookResp();
+//      BeanUtils.copyProperties(ebook,ebookResq);
+//      resqList.add(ebookResq);
+//    }
+    List<EbookResp> resqList = CopyUtil.copyList(ebookList, EbookResp.class);
     return resqList;
   }
 }
