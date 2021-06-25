e311f4630b29ece36059f1f19e90c93a92a75dc1
tengda <303571072@qq.com>
2021-06-25 22:54:47 +0200

    3-7 Encapsulation of request & response params

--- src/main/java/com/tengda/wiki/controller/EbookController.java
+++ src/main/java/com/tengda/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.tengda.wiki.controller;
 
-import com.tengda.wiki.domain.Ebook;
+import com.tengda.wiki.req.EbookReq;
 import com.tengda.wiki.resp.CommonResp;
+import com.tengda.wiki.resp.EbookResp;
 import com.tengda.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +22,9 @@ public class EbookController {
 //    return ebookService.list();
 //  }
   @GetMapping ("/list")
-  public CommonResp list() {
-    CommonResp<List<Ebook>>  resp = new CommonResp<>();
-    List<Ebook> list = ebookService.list();
+  public CommonResp list(EbookReq req) {
+    CommonResp<List<EbookResp>>  resp = new CommonResp<>();
+    List<EbookResp> list = ebookService.list(req);
     resp.setContent(list);
     return resp;
   }
--- src/main/java/com/tengda/wiki/service/EbookService.java
+++ src/main/java/com/tengda/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.tengda.wiki.service;
 
 import com.tengda.wiki.domain.Ebook;
+import com.tengda.wiki.domain.EbookExample;
 import com.tengda.wiki.mapper.EbookMapper;
+import com.tengda.wiki.req.EbookReq;
+import com.tengda.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,7 +17,17 @@ public class EbookService {
   @Resource
   private EbookMapper ebookMapper;
 
-  public List<Ebook> list() {
-    return ebookMapper.selectByExample(null);
+  public List<EbookResp> list(EbookReq req) {
+    EbookExample ebookExample = new EbookExample();
+    EbookExample.Criteria criteria = ebookExample.createCriteria();
+    criteria.andNameLike("%" + req.getName() + "%");
+    List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+    List<EbookResp> resqList = new ArrayList<>();
+    for (Ebook ebook: ebookList) {
+      EbookResp ebookResq = new EbookResp();
+      BeanUtils.copyProperties(ebook,ebookResq);
+      resqList.add(ebookResq);
+    }
+    return resqList;
   }
 }
